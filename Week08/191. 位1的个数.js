9d1e02e73b7fde543f1f05abe8c58836bb46680e
shan <949520511@qq.com>
2020-08-02 23:53:02 +0800

    add

--- Week08/191. 位1的个数.js
+++ Week08/191. 位1的个数.js
@@ -2,14 +2,14 @@
  * @param {number} n - a positive integer
  * @return {number}
  */
-var hammingWeight = function(n) {
-    let count = 0;
-    let mask = 1;
-    for(let i = 0;i < 32;i++){
-        if((n & mask) != 0){
-            count++;
-        }
-        mask <<= 1;
+var hammingWeight = function (n) {
+  let count = 0;
+  let mask = 1;
+  for (let i = 0; i < 32; i++) {
+    if ((n & mask) != 0) {
+      count++;
     }
-    return count;
+    mask <<= 1;
+  }
+  return count;
 };
